ae8243edd61f0dd7320894a00680fc4eaccd530e
wyb <963640324@qq.com>
2020-04-07 14:17:39 +0800

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import VueResource from 'vue-resource'
+import axios from 'axios'
+Vue.use(VueResource)
+Vue.use(ElementUi)
 Vue.use(VueRouter);
+Vue.http.options.emulateJSON = true;
 
 const routes = [
   {
